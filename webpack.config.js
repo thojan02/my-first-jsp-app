d5d067cf8e28aeb269d8a9c3ee4325d769f7cc34
Jan-Anders Thorsveen <jan-anders.thorsveen@tine.no>
2016-06-28 11:00:13 +0200

    Added webpack config with first webpacked module and export/output definition

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+```js
+/**
+ * /webpack.config.js
+ */
+
+var webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    './src/entry.js'
+  ],
+
+  output: {
+    path: './public/',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+};
+```
