ffcf1e2718ee40f41d64b3bc5487cfce36b62fbd
Jan-Anders Thorsveen <jan-anders.thorsveen@tine.no>
2016-06-28 16:12:34 +0200

    some production readyfication

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,36 @@
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
+/**
+ * Check if we are running production mode
+ */
+var isProduction = process.env.NODE_ENV == 'production';
+
+/**
+ * Create an array of plugins that
+ * we'll use in our webpack config
+ */
+var plugins = [];
+
+/**
+ * Forward process.env.NODE_ENV into the bundle
+ * to enable React.js production mode
+ */
+plugins.push(new webpack.DefinePlugin({
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+}))
+
+/**
+ * Do code de-duping and minification
+ * when running production mode
+ */
+if (isProduction) {
+    plugins.push(new webpack.optimize.DedupePlugin());
+    plugins.push(new webpack.optimize.UglifyJsPlugin());
+}
+
 module.exports = {
-  devtool: 'inline-source-map',
+  devtool: isProduction ? 'source-map' : 'inline-source-map',
 
   entry: [
     './src/entry.js'
