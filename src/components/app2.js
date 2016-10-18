9c9511bfdd7120566c4a84ab4c052eb599b3020a
Jan-Anders Thorsveen <jan-anders.thorsveen@tine.no>
2016-10-18 19:35:45 +0200

    Some tweaks, and added a county map

--- src/components/app2.js
+++ src/components/app2.js
@@ -5,7 +5,8 @@
 import React from 'react';
 import Header from './header.js';
 import Map from './map.js'
-var mapImage = require('../resources/Norway_municipalities_2012_blank.svg');
+var municipalityMapImage = require('../resources/Norway_municipalities_2012_blank.svg');
+var countyMapImage = require('../resources/Norway_counties_blank.svg');
 
 class App2 extends React.Component {
     constructor(props) {
@@ -15,11 +16,22 @@ class App2 extends React.Component {
     render() {
         console.log('In App2 render');
 
+        const municipalStyle = {
+            float: 'left',
+            width: '50%'
+        };
+
+        const countyStyle = {
+            float: 'left',
+            width: '50%'
+        };
+
         return (
             React.createElement('div', {},
-                React.createElement(Header, {title: 'A simple Nowegian municipality map app', ingress: 'This is the first small steps toward a fully Elasticsearch backed statistics view.'}),
+                React.createElement(Header, {title: 'A simple Nowegian municipality and county map app', ingress: 'This is the first small steps toward a fully Elasticsearch backed statistics view.'}),
                 React.createElement('hr', null),
-                React.createElement(Map, {title: 'Norgeskart med kommuner', alt:'Kart over Norges kommuner', mapFile:mapImage})
+                React.createElement(Map, {mapStyle:municipalStyle, title: 'Norgeskart med kommuner', alt:'Kart over Norges kommuner', mapFile:municipalityMapImage}),
+                React.createElement(Map, {mapStyle:countyStyle ,title: 'Norgeskart med fylker', alt:'Kart over Norges fylker', mapFile:countyMapImage})
             )
         );
 
--- src/components/map.js
+++ src/components/map.js
@@ -13,15 +13,22 @@ class Map extends React.Component {
     render() {
         console.log('In Map render');
 
+        const mapStyle = {
+            background: 'PowderBlue',
+            height: '80%',
+            width: '80%',
+            align: 'centre'
+        };
 
-        return React.createElement('div', {},
+        return React.createElement('div', {style: this.props.mapStyle},
             React.createElement('h2', {}, this.props.title),
-            React.createElement('img', {src: mapImage, alt: this.props.alt})
+            React.createElement('img', {src: this.props.mapFile, alt: this.props.alt, style: mapStyle})
         );
     }
 }
 
 Map.propTypes = {
+    mapStyle: React.PropTypes.string.isRequired,
     title: React.PropTypes.string.isRequired,
     alt: React.PropTypes.string.isRequired,
     mapFile: React.PropTypes.string.isRequired
